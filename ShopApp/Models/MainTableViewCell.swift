8e65dd959a50e330d65365376decf8dda81b451c
zr2x <master5212345@gmail.com>
2023-08-27 21:08:26 +0500

    Refactor naming

--- ShopApp/Models/MainTableViewCell.swift
+++ ShopApp/Models/MainTableViewCell.swift
@@ -14,17 +14,18 @@ class MainTableViewCell: UITableViewCell {
     private var stackView = UIStackView()
     
     // MARK: - Init
-//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//        addViews()
-//        configureStackVIew()
-//        configureLayout()
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        
+    }
     func configureViews(product: Product) {
         separatorInset = UIEdgeInsets(top: 15, left: 55, bottom: 0, right: 0)
         
